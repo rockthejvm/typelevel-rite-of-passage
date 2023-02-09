6d8ceaec277d13ee16353f0482096bfa95689d79
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2023-02-09 16:22:11 +0200

    jobs endpoints

--- src/main/scala/com/rockthejvm/jobsboard/Application.scala
+++ src/main/scala/com/rockthejvm/jobsboard/Application.scala
@@ -10,28 +10,25 @@ import cats.*
 import cats.implicits.*
 import cats.effect.IO
 import org.http4s.ember.server.EmberServerBuilder
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-import com.rockthejvm.jobsboard.http.routes.HealthRoutes
+import com.rockthejvm.jobsboard.http.HttpApi
 import com.rockthejvm.jobsboard.config.*
 import com.rockthejvm.jobsboard.config.syntax.*
 import pureconfig.ConfigSource
 import pureconfig.error.ConfigReaderException
 
-/*
-  1 - add a plain health endpoint to our app
-  2 - add minimal configuration
-  3 - basic http server layout
- */
 object Application extends IOApp.Simple {
 
-  val configSource = ConfigSource.default.load[EmberConfig]
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   override def run = ConfigSource.default.loadF[IO, EmberConfig].flatMap { config =>
     EmberServerBuilder
       .default[IO]
       .withHost(config.host)
       .withPort(config.port)
-      .withHttpApp(HealthRoutes[IO].routes.orNotFound)
+      .withHttpApp(HttpApi[IO].endpoints.orNotFound)
       .build
       .use(_ => IO.println("Rock the JVM!") *> IO.never)
   }
---
+++ src/main/scala/com/rockthejvm/jobsboard/http/routes/JobRoutes.scala
@@ -0,0 +1,93 @@
+package com.rockthejvm.jobsboard.http.routes
+
+import io.circe.generic.auto.*
+import org.http4s.circe.CirceEntityCodec.*
+
+import org.http4s.*
+import org.http4s.dsl.*
+import org.http4s.dsl.impl.*
+import org.http4s.server.*
+import cats.effect.*
+import cats.implicits.*
+import org.typelevel.log4cats.Logger
+
+import scala.collection.mutable
+import java.util.UUID
+import com.rockthejvm.jobsboard.domain.job.*
+import com.rockthejvm.jobsboard.http.responses.*
+
+class JobRoutes[F[_]: Concurrent: Logger] private extends Http4sDsl[F] {
+
+  // "database"
+  private val database = mutable.Map[UUID, Job]()
+
+  // POST /jobs?offset=x&limit=y { filters } // TODO add query params and filters
+  private val allJobsRoute: HttpRoutes[F] = HttpRoutes.of[F] { case POST -> Root =>
+    Ok(database.values)
+  }
+
+  // GET /jobs/uuid
+  private val findJobRoute: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root / UUIDVar(id) =>
+    database.get(id) match {
+      case Some(job) => Ok(job)
+      case None      => NotFound(FailureResponse(s"Job $id not found."))
+    }
+  }
+
+  // POST /jobs/create { jobInfo }
+  private def createJob(jobInfo: JobInfo): F[Job] =
+    Job(
+      id = UUID.randomUUID(),
+      date = System.currentTimeMillis(),
+      ownerEmail = "TODO@rockthejvm.com",
+      jobInfo = jobInfo,
+      active = true
+    ).pure[F]
+
+  import com.rockthejvm.jobsboard.logging.syntax.*
+
+  private val createJobRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case req @ POST -> Root / "create" =>
+      for {
+        jobInfo <- req.as[JobInfo].logError(e => s"Parsing payload failed: $e")
+        job     <- createJob(jobInfo)
+        _       <- database.put(job.id, job).pure[F]
+        resp    <- Created(job.id)
+      } yield resp
+  }
+
+  // PUT /jobs/uuid { jobInfo }
+  private val updateJobRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case req @ PUT -> Root / UUIDVar(id) =>
+      database.get(id) match {
+        case Some(job) =>
+          for {
+            jobInfo <- req.as[JobInfo]
+            _       <- database.put(id, job.copy(jobInfo = jobInfo)).pure[F]
+            resp    <- Ok()
+          } yield resp
+        case None => NotFound(FailureResponse(s"Cannot update job $id: not found"))
+      }
+  }
+
+  // DELETE /jobs/uuid
+  private val deleteJobRoute: HttpRoutes[F] = HttpRoutes.of[F] {
+    case req @ DELETE -> Root / UUIDVar(id) =>
+      database.get(id) match {
+        case Some(job) =>
+          for {
+            _    <- database.remove(id).pure[F]
+            resp <- Ok()
+          } yield resp
+        case None => NotFound(FailureResponse(s"Cannot delete job $id: not found"))
+      }
+  }
+
+  val routes = Router(
+    "/jobs" -> (allJobsRoute <+> findJobRoute <+> createJobRoute <+> updateJobRoute <+> deleteJobRoute)
+  )
+}
+
+object JobRoutes {
+  def apply[F[_]: Concurrent: Logger] = new JobRoutes[F]
+}
