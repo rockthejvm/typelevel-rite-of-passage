3bd490a8143c427468b67bf05ac8028f702103b8
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2023-03-06 17:53:29 +0200

    header

--- app/src/main/scala/com/rockthejvm/jobsboard/App.scala
+++ app/src/main/scala/com/rockthejvm/jobsboard/App.scala
@@ -8,6 +8,7 @@ import org.scalajs.dom.window
 import scala.concurrent.duration.*
 
 import core.*
+import components.*
 
 object App {
   type Msg = Router.Msg
@@ -40,22 +41,8 @@ class App extends TyrianApp[App.Msg, App.Model] {
   // view triggered whenever model changes
   override def view(model: Model): Html[Msg] =
     div(
-      renderNavLink("Jobs", "/jobs"),
-      renderNavLink("Login", "/login"),
-      renderNavLink("Sign Up", "/signup"),
+      Header.view(),
       div(s"You are now at: ${model.router.location}")
     )
 
-  private def renderNavLink(text: String, location: String) =
-    a(
-      href    := location,
-      `class` := "nav-link",
-      onEvent(
-        "click",
-        e => {
-          e.preventDefault() // native JS - prevent reloading the page
-          Router.ChangeLocation(location)
-        }
-      )
-    )(text)
 }
---
+++ app/src/main/scala/com/rockthejvm/jobsboard/components/Header.scala
@@ -0,0 +1,62 @@
+package com.rockthejvm.jobsboard.components
+
+import tyrian.*
+import tyrian.Html.*
+import scala.scalajs.js
+import scala.scalajs.js.annotation.*
+
+import com.rockthejvm.jobsboard.core.*
+
+object Header {
+
+  // public API
+  def view() =
+    div(`class` := "header-container")(
+      renderLogo(),
+      div(`class` := "header-nav")(
+        ul(`class` := "header-links")(
+          renderNavLink("Jobs", "/jobs"),
+          renderNavLink("Login", "/login"),
+          renderNavLink("Sign Up", "/signup")
+        )
+      )
+    )
+
+  // private API
+  @js.native
+  @JSImport("/static/img/fiery-lava 128x128.png", JSImport.Default)
+  private val logoImage: String = js.native
+
+  private def renderLogo() =
+    a(
+      href := "/",
+      onEvent(
+        "click",
+        e => {
+          e.preventDefault()
+          Router.ChangeLocation("/")
+        }
+      )
+    )(
+      img(
+        `class` := "home-logo",
+        src     := logoImage,
+        alt     := "Rock the JVM"
+      )
+    )
+
+  private def renderNavLink(text: String, location: String) =
+    li(`class` := "nav-item")(
+      a(
+        href    := location,
+        `class` := "nav-link",
+        onEvent(
+          "click",
+          e => {
+            e.preventDefault() // native JS - prevent reloading the page
+            Router.ChangeLocation(location)
+          }
+        )
+      )(text)
+    )
+}
