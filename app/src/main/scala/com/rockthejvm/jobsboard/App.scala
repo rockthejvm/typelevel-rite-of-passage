56d84f59f33e0a60fa6565e67f8fa4c2d0dabf65
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2023-03-06 15:24:36 +0200

    routing

--- app/src/main/scala/com/rockthejvm/jobsboard/App.scala
+++ app/src/main/scala/com/rockthejvm/jobsboard/App.scala
@@ -4,45 +4,58 @@ import tyrian.*
 import tyrian.Html.*
 import cats.effect.*
 import scala.scalajs.js.annotation.*
-import org.scalajs.dom.document
+import org.scalajs.dom.window
 import scala.concurrent.duration.*
 
-object App {
-  sealed trait Msg
-  case class Increment(amount: Int) extends Msg
+import core.*
 
-  case class Model(count: Int)
+object App {
+  type Msg = Router.Msg
+
+  case class Model(router: Router)
 }
 
 @JSExportTopLevel("RockTheJvmApp")
 class App extends TyrianApp[App.Msg, App.Model] {
   import App.*
-  /*
-    We can send messages by
-    - trigger a command
-    - create a subscription
-    - listening for an event
-   */
 
-  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
-    (Model(0), Cmd.None)
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) = {
+    val (router, cmd) = Router.startAt(window.location.pathname)
+    (Model(router), cmd)
+  }
 
-  // potentially endless stream of messages
   override def subscriptions(model: Model): Sub[IO, Msg] =
-    Sub.every[IO](1.second).map(_ => Increment(1))
+    Sub.make( // listener for browser history changes
+      "urlChange",
+      model.router.history.state.discrete
+        .map(_.get)
+        .map(newLocation => Router.ChangeLocation(newLocation, true))
+    )
 
-  // model can change by receiving messages
-  // model => message => (new model, ______)
-  // update triggered whenever we get a new message
-  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case Increment(amount) =>
-    (model.copy(count = model.count + amount), Cmd.None)
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case msg: Router.Msg =>
+    val (newRouter, cmd) = model.router.update(msg)
+    (model.copy(router = newRouter), cmd)
   }
 
   // view triggered whenever model changes
   override def view(model: Model): Html[Msg] =
     div(
-      button(onClick(Increment(1)))("increase"),
-      button(onClick(Increment(-1)))("decrease"),
-      div(s"Tyrian running: ${model.count}")
+      renderNavLink("Jobs", "/jobs"),
+      renderNavLink("Login", "/login"),
+      renderNavLink("Sign Up", "/signup"),
+      div(s"You are now at: ${model.router.location}")
     )
+
+  private def renderNavLink(text: String, location: String) =
+    a(
+      href    := location,
+      `class` := "nav-link",
+      onEvent(
+        "click",
+        e => {
+          e.preventDefault() // native JS - prevent reloading the page
+          Router.ChangeLocation(location)
+        }
+      )
+    )(text)
 }
---
+++ app/src/main/scala/com/rockthejvm/jobsboard/playground/PlaygroundApp.scala
@@ -0,0 +1,47 @@
+package com.rockthejvm.jobsboard.playground
+
+import tyrian.*
+import tyrian.Html.*
+import cats.effect.*
+import scala.scalajs.js.annotation.*
+import org.scalajs.dom.document
+import scala.concurrent.duration.*
+
+object PlaygroundApp {
+  sealed trait Msg
+  case class Increment(amount: Int) extends Msg
+
+  case class Model(count: Int)
+}
+
+class PlaygroundApp extends TyrianApp[PlaygroundApp.Msg, PlaygroundApp.Model] {
+  import PlaygroundApp.*
+  /*
+    We can send messages by
+    - trigger a command
+    - create a subscription
+    - listening for an event
+   */
+
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    (Model(0), Cmd.None)
+
+  // potentially endless stream of messages
+  override def subscriptions(model: Model): Sub[IO, Msg] =
+    Sub.every[IO](1.second).map(_ => Increment(1))
+
+  // model can change by receiving messages
+  // model => message => (new model, ______)
+  // update triggered whenever we get a new message
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case Increment(amount) =>
+    (model.copy(count = model.count + amount), Cmd.None)
+  }
+
+  // view triggered whenever model changes
+  override def view(model: Model): Html[Msg] =
+    div(
+      button(onClick(Increment(1)))("increase"),
+      button(onClick(Increment(-1)))("decrease"),
+      div(s"Tyrian running: ${model.count}")
+    )
+}
